12c1a12a141684a5b1364e625262996adcf9d19c
Ramprosad Gharami <ram.bsmrstu@gmail.com>
2025-05-07 00:27:21 +0600

    Fix: Implemented challenges

--- src/app/user-agent/page.tsx
+++ src/app/user-agent/page.tsx
@@ -1,7 +1,15 @@
+import { headers } from "next/headers";
+import { UserAgentProvider } from "@/components/providers/userAgentProvider";
 import { UserAgent } from "@/views/userAgent";
 
 const UserAgentRoot = () => {
-  return <UserAgent />;
-};
+  const userAgent = headers().get("user-agent") || undefined;
+
+  return (
+    <UserAgentProvider userAgent={userAgent}>
+      <UserAgent />
+    </UserAgentProvider>
+  );
+}
 
 export default UserAgentRoot;
--- src/views/products/productList/productList.tsx
+++ src/views/products/productList/productList.tsx
@@ -1,23 +1,59 @@
-import React from "react";
-import { Product } from "@/types";
+'use client';
+
+import React, { useCallback, useEffect, useState } from 'react';
+import { Product } from '@/types';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { ProductModal } from '../productModal';
 
 interface ProductListProps {
   products: Product[];
-  onOpenModal: (product: Product) => void;
 }
 
-export const ProductList: React.FC<ProductListProps> = ({
-  products,
-  onOpenModal,
-}) => (
-  <div>
-    {products.map((product) => (
-      <div key={product.id} className="flex border p-2 justify-between">
-        <div className="flex">
-          <div>{product.id}</div>. {product.name}
+export const ProductList: React.FC<ProductListProps> = ({ products }) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const productId = searchParams.get('product-id');
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+
+  const handleCloseModal = useCallback(() => {
+    setSelectedProduct(null);
+    const params = new URLSearchParams(searchParams.toString());
+    params.delete('product-id');
+    router.push(`/products?${params.toString()}`);
+  }, [router, searchParams]);
+
+  useEffect(() => {
+    if (productId) {
+      const openedProduct = products.find((p) => p.id.toString() === productId);
+      if (openedProduct) {
+        setSelectedProduct(openedProduct);
+      }
+    }
+    else {
+      setSelectedProduct(null);
+    }
+  }, [productId, products]);
+
+  const handleOpenModal = (product: Product) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set('product-id', product.id.toString());
+    router.push(`/products?${params.toString()}`);
+  };
+
+  return (
+    <div>
+      {products.map((product) => (
+        <div key={product.id} className="flex border p-2 justify-between">
+          <div className="flex">
+            <div>{product.id}</div>. {product.name}
+          </div>
+          <button onClick={() => handleOpenModal(product)}>Details</button>
         </div>
-        <button onClick={() => onOpenModal(product)}>Details</button>
-      </div>
-    ))}
-  </div>
-);
+      ))}
+
+      {selectedProduct && (
+        <ProductModal product={selectedProduct} onClose={handleCloseModal} />
+      )}
+    </div>
+  );
+};
